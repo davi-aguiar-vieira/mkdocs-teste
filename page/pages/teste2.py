fc34f58a691414a68c185d7a00af089421299527
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2024-07-01 13:50:22 -0300

    teste3

--- page/pages/teste2.py
+++ page/pages/teste2.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Página 2 de testes")
+st.title("Página 2 de testes")
+st.title("Página 2 de testes")
+st.title("Página 2 de testes")
+st.title("Página 2 de testes")
